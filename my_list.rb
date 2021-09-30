73116a33b7482888269e6b4051a842009868ca1e
Ghiftee <gigibetine@gmail.com>
2021-09-30 15:48:12 +0100

    Fix rubocop errors

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,4 @@
-require_relative 'MyEnumerable'
+require_relative 'my_enumerable'
 
 class MyList
   include Enumerables
@@ -16,11 +16,11 @@ class MyList
   end
 end
 
-list = MyList.new(1, 2, 3, 4, 9)
+list = MyList.new(1, 2, 3, 4)
 list.all? { |e| e < 5 }
 list.all? { |e| e > 5 }
 
 list.any? { |e| e == 2 }
 list.any? { |e| e == 5 }
 
-list.filter { |e| e.even? }
+list.filter(&:even?)
